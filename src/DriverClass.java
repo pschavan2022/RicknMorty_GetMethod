2333ea44117e34ab51497cb6d3d99c56855bd6eb
pschavan2022 <you@example.com>
2023-05-17 05:36:26 +0530

    First commit of AutomationFramework

---
+++ src/DriverClass.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+import testClassPackage.post_tc_1;
+
+public class DriverClass {
+	
+	public static void main(String[] args) throws IOException {
+		
+			post_tc_1.execute();
+
+	}
+
+}
+	
